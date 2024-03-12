649b138a871165e4ece6913da0668751b08f276d
marc-i <marc-alexander.iten@fhgr.ch>
2024-03-12 08:11:02 +0100

    bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import moment from 'moment';
 import {AreasplineComponent} from './components/charts/areaspline/areaspline.component';
 import {LineComponent} from './components/charts/line/line.component';
 import {PieComponent} from './components/charts/pie/pie.component';
-import {BoxplotComponent} from './components/charts/boxplot/boxplot.component';
 
 @Component({
     selector: 'app-root',
@@ -40,16 +39,6 @@ export class AppComponent {
     title_pvverbrauch = 'Eigenverbrauch';
     label_pvverbrauch = EnergyEntryService.MonthEnergyEntries.map(e => moment(e.Datum).format('MMM YY'));
     data_pvverbrauch = EnergyEntryService.MonthEnergyEntries.map(e => e.PVVerbrauchSumme);
-
-    title_batterie = 'Box-Plot';
-    label_batterie = ['1', '2', '3', '4', '5'];
-    data_batterie = [
-        [760, 801, 848, 895, 965],
-        [733, 853, 939, 980, 1080],
-        [714, 762, 817, 870, 918],
-        [724, 802, 806, 871, 950],
-        [834, 836, 864, 882, 910]
-    ];
 
     constructor() {
     }
